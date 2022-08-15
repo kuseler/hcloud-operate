aca4405c2da18d291b53c532f7e0122e886e327d
kuseler <47039588+kuseler@users.noreply.github.com>
2022-08-15 11:31:36 +0200

    Add files via upload

--- sshOperator/sshOperator.go
+++ sshOperator/sshOperator.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/hetznercloud/hcloud-go/hcloud"
 )
@@ -21,20 +22,30 @@ func createServer(name string, locationIDOrName string, serverTypeName string, i
 	if userdata != "" {
 		serverOpts.UserData = userdata
 	}
-	createKey(name+"-publicKey", publicKey)
-	publicKeySSH, _, _ := client.SSHKey.Get(context.Background(), name+"-publicKey")
-	serverOpts.SSHKeys = append(serverOpts.SSHKeys, publicKeySSH)
 	// validation of server options
 	err := serverOpts.Validate()
 	if err != nil {
 		fmt.Printf("Error during validation: %v\n", err)
 		return
 	}
+	// if the other server options are correct, we create the publicKey
+	createKey(name+"-Key", publicKey)
+	publicKeySSH, _, _ := client.SSHKey.Get(context.Background(), name+"-Key")
+	serverOpts.SSHKeys = append(serverOpts.SSHKeys, publicKeySSH)
+
 	result, _, err := client.Server.Create(context.Background(), serverOpts)
 	if err != nil {
-		fmt.Printf("%v", err)
+		fmt.Printf("Error during creation: %v", err)
+		fmt.Println(result)
 	}
-	fmt.Println(result)
+}
+
+func testCreationTime() {
+	start := time.Now()
+	createServer("abc", "flk1", "cx11", "ubuntu-20.04", "", "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1ORh6h8PpZ57zzx0rYBS/WjRu7ObAws6dSN+xQ5zcC1VZo2H/yJdcuyUU8HObkRZHRBTaMEbh3W3nnWj1PggeO7BQxUsLhtuSneI8FvIodbmYsyvAigReyv5pxfj9N0o06oCvkDP/kFTgidcAt1kUvBcSQfT97KltGYo4i+zVt6U+YCaeHOZTz7R11tHaOeh7b7A4z2olwcrhrfzq+s55WumvH0sM+Ohfh6Xo0FYgoO/G4XCLeymdYPbAA1JU96qarHF0sFBTv0zdCNl/grK2im4D4giSCjsYdxU9xFYLgsj8QIBZeAvQ7RSZTtlgh1IKsBvuQHBTwOzlVsb3YzJFVOI053TnMinhrJjJCtIWJYpVCW6QNNkMnCtiU+SAD0PKdX0uFF4Gy5/9K2m4PfPgyvtrjusPEGgkt3+BeKgbZHhoX8efktVBaj/aph0PUum3VkSPfBbduISsypl2cXCIOeTshBg3zPQxptK9qepMF1DY8JkRgQNSjcjPWy0MrLlAaG/UiUvgeFXhr6Hi5paIZ9bzSv1V66MNHvlxW3HXj4LtQjbZnDFfLo/pK+fMjSwW4ZDewgvYPrevMFvxEansEPbAIPvd0SYCjbRyOdSRH7hNH1bOapxiSZTD1Ja1P4umbRe1RXyRBgx02T7sAKvqJkUqpkgwDbowi6TxdTEXuQ== kimi@kimiarch")
+	elapsed := time.Since(start)
+	fmt.Printf("Creation of Server took %s", elapsed)
+	//deleteServer("abc")
 }
 
 /*
@@ -51,20 +62,22 @@ Was ist schneller? Docker über cloudinit zsh oder Docker über snapshot
 
 func deleteServer(name string) {
 	client := hcloud.NewClient(hcloud.WithToken(os.Getenv("API_TOKEN")))
-	serverToBeDeleted, _, _ := client.Server.Get(context.Background(), name)
+	serverToBeDeleted, _, err := client.Server.Get(context.Background(), name)
+	if err != nil {
+		fmt.Println(err)
+	}
+	deleteKey(name + "-Key")
 	client.Server.Delete(context.Background(), serverToBeDeleted)
-	deleteKey(name + "-publicKey")
 }
 
 func createKey(name, publicKey string) {
 	labels := make(map[string]string)
 	client := hcloud.NewClient(hcloud.WithToken(os.Getenv("API_TOKEN")))
 	SSHKeyCreateOpts := hcloud.SSHKeyCreateOpts{Name: name, PublicKey: publicKey, Labels: labels}
-	SSHKey, _, error := client.SSHKey.Create(context.Background(), SSHKeyCreateOpts) //create SSHKey
+	_, _, error := client.SSHKey.Create(context.Background(), SSHKeyCreateOpts) //create SSHKey
 	if error != nil {
 		fmt.Printf("Error while creating sshKey: %v", error) //Print out SSHKey
 	}
-	fmt.Printf("%v\n", SSHKey)
 
 }
 
@@ -96,7 +109,7 @@ func main() {
 		PublicKey := os.Args[3]
 		createKey(name, PublicKey)
 	case *serv:
-		createServer("abc", "nbg1", "cx11", "79028095", "", "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1ORh6h8PpZ57zzx0rYBS/WjRu7ObAws6dSN+xQ5zcC1VZo2H/yJdcuyUU8HObkRZHRBTaMEbh3W3nnWj1PggeO7BQxUsLhtuSneI8FvIodbmYsyvAigReyv5pxfj9N0o06oCvkDP/kFTgidcAt1kUvBcSQfT97KltGYo4i+zVt6U+YCaeHOZTz7R11tHaOeh7b7A4z2olwcrhrfzq+s55WumvH0sM+Ohfh6Xo0FYgoO/G4XCLeymdYPbAA1JU96qarHF0sFBTv0zdCNl/grK2im4D4giSCjsYdxU9xFYLgsj8QIBZeAvQ7RSZTtlgh1IKsBvuQHBTwOzlVsb3YzJFVOI053TnMinhrJjJCtIWJYpVCW6QNNkMnCtiU+SAD0PKdX0uFF4Gy5/9K2m4PfPgyvtrjusPEGgkt3+BeKgbZHhoX8efktVBaj/aph0PUum3VkSPfBbduISsypl2cXCIOeTshBg3zPQxptK9qepMF1DY8JkRgQNSjcjPWy0MrLlAaG/UiUvgeFXhr6Hi5paIZ9bzSv1V66MNHvlxW3HXj4LtQjbZnDFfLo/pK+fMjSwW4ZDewgvYPrevMFvxEansEPbAIPvd0SYCjbRyOdSRH7hNH1bOapxiSZTD1Ja1P4umbRe1RXyRBgx02T7sAKvqJkUqpkgwDbowi6TxdTEXuQ== kimi@kimiarch")
+		testCreationTime()
 	case *del:
 		deleteServer("abc")
 	default:
